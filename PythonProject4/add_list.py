5071628f42c02a982baeca7545a11bc66a4ea714
SaMsep10l12 <vanekklass5@gmail.com>
2025-01-24 18:22:31 +0500

    Add files via upload

---
+++ PythonProject4/add_list.py
@@ -0,0 +1,22 @@
+username = input("Введите имя пользователя: ")
+content = input("Введите описание заметки: ")
+status = input("Введите статус заметки (например, 'Активна', 'Выполнена'): ")
+created_date = input("Введите дату создания заметки в формате 'день-месяц-год': ")
+issue_date = input("Введите дату истечения заметки в формате 'день-месяц-год': ")
+
+
+
+titles = []
+for title in range(3):
+    title = input(f"Введите заголовок заметки {title + 1}: ")
+    titles.append(title)
+
+
+
+print("\nВы ввели следующие данные:")
+print("Имя пользователя:", username)
+print("Заголовки заметки:", titles)
+print("Описание заметки:", content)
+print("Статус заметки:", status)
+print("Дата создания заметки:", created_date)
+print("Дата истечения заметки:", issue_date)
